61295d4b1998aa1935f84fd3888a6f487092a177
FreshMag <pescomagnani@gmail.com>
2026-02-25 21:27:33 +0100

    feat: added support for local models

---
+++ src/paas_lab/utils/local_client.py
@@ -0,0 +1,11 @@
+from datapizza.clients.openai_like import OpenAILikeClient
+
+client = OpenAILikeClient(
+    api_key="", # Ollama doesn't require an API key
+    model="deepseek-r1:8b",
+    base_url="http://localhost:11434/v1",
+    #system_prompt=".",
+)
+
+response = client.invoke("What is the capital of France?")
+print(response.content)
