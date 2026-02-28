9fe507a16ab7709fa7461c346b7327613ed7d99a
Luca Rubboli <luca.rubboli@libero.it>
2026-02-28 12:41:34 +0100

    chore: add client template and regolo client

--- src/paas_lab/utils/openai_client.py
+++ src/paas_lab/utils/openai_client.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from datapizza.clients.openai import OpenAIClient
 from datapizza.embedders.openai import OpenAIEmbedder
+from datapizza.clients.openai_like import OpenAILikeClient
 
 load_dotenv()
 
@@ -13,6 +14,18 @@ openai_client = OpenAIClient(
     model=GPT_MODEL,
     # system_prompt="You are a helpful assistant",
     # temperature=0.7 # controls randomness (0-2)
+)
+
+regolo_client_120 = OpenAILikeClient(
+    api_key=os.getenv("REGOLO_KEY"),
+    model="gpt-oss-120b",
+    base_url="https://api.regolo.ai/v1",
+)
+
+regolo_client_20 = OpenAILikeClient(
+    api_key=os.getenv("REGOLO_KEY"),
+    model="gpt-oss-20b",
+    base_url="https://api.regolo.ai/v1",
 )
 
 embedder = OpenAIEmbedder(api_key=OPENAI_KEY, model_name="text-embedding-3-small")
